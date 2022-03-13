5b024beedb1ac6734cec6dd995621108706fbe0d
jolson2811 <jolson2811@gmail.com>
2022-03-12 17:37:01 -0700

    added formatTime function, loop for time block html, background color function

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,43 @@
 $(document).ready(function () {
-    today = moment().format('LLLL');
-    console.log(today);
+    var today = moment().format('LLLL');
     $("#currentDay").text(today);
 
+    var start = 9;
+    var end = 24;
+
+    var d = new Date(); 
+    var dhour = d.getHours();
+
+    function formatTime(hour) {
+        var formatHour = null;
+        if (hour > 12) {
+            formatHour = hour - 12;
+        } else {
+            formatHour = hour;
+        }
+        if (hour >= 12) {
+            formatHour = formatHour + "PM";
+        } else {
+            formatHour = formatHour + "AM";
+        } 
+        return formatHour;
+    }
+
+    function backgroundColor(hour) {
+        if (dhour == hour) {
+            return "bg-danger";
+        } else if (dhour < hour) {
+            return "bg-success";
+        } else {
+            return "bg-light";
+        }
+    }
+
+    var innerHTML = "";
+
+    for (var i = start; i <= end; i++) {
+        innerHTML += '<div id="'+formatTime(i)+'" class="row"><div class="col-sm-2 text-right align-text-top border border-secondary rounded pt-2">'+formatTime(i)+'</div><textarea id="'+formatTime(i)+'-task" class="col-sm-8 '+backgroundColor(i)+' text-left align-text-top border border-secondary pt-2"></textarea><button id="'+formatTime(i)+'-save" type="button" class="col-sm-2 btn btn-info border border-secondary rounded">Save</button></div>';
+    }
+    $("#tasks").html(innerHTML);
 });
+
